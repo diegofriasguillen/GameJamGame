e1f920a4c4899604ac2afef04a283f769b0b6ac6
Salud3 <78719641+Salud3@users.noreply.github.com>
2024-10-20 08:06:06 -0600

    Bat

---
+++ Assets/Scripts/UI Barras/BatFunction.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatFunction : MonoBehaviour
+{
+    public bool active;
+    public bool isunique;
+    public GameObject b1;
+    public GameObject b2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !active)
+        {
+            BatFunction bat = other.GetComponentInChildren<BatFunction>();
+            bat.transform.gameObject.SetActive(true);
+            bat.ActiveModel();
+            this.gameObject.SetActive(false);
+            active = true;
+        }
+    }
+
+    public void ActiveModel()
+    {
+        b1.SetActive(true);
+        b2.SetActive(true);
+        isunique = true;
+        active = true;
+    } 
+
+}
